c11193529cdd6504b2b7736f17a495a5a5ec2a16
Axel <apple@Apples-MacBook-Pro.local>
2023-02-06 11:29:56 +0200

    crypto app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./Routes/Home";
+import CoinPage from "./Routes/CoinPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/" element={<CoinPage />}></Route>
+      </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Coin.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../Components/Coin.css";
+import { useNavigate } from 'react-router-dom';
+
+const Coin = ({
+  icon,
+  coinName,
+  coinSymbol,
+  price,
+  marketCap,
+  priceChange,
+  id,
+}) => {
+    const navigate = useNavigate();
+  return (
+    <div className="coinContainer">
+      <div className="coinRow">
+        <div className="coinData">
+          <div className="coin">
+            <img src={icon} />
+            <h1 className="coinName">{coinName}</h1>
+            <p className="coinSymbol">{coinSymbol}</p>
+            <p className="coinPrice">$ {price.toFixed(2)}</p>
+            {priceChange < 0 ? (
+              <p className="priceChange red">{priceChange.toFixed(2)}%</p>
+            ) : (
+              <p className="priceChange green">{priceChange.toFixed(2)}%</p>
+            )}
+            <p className="coinVolume">$ {marketCap.toLocaleString()}</p>
+            <button
+              onClick={() => {
+                navigate(`/CoinPage/${id}`);
+              }}
+            >
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
